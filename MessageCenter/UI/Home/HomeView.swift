47542acae4a62a00ec853e882121015ca06878f0
jmuthialu <jmuthialu@yahoo.com>
2024-06-18 22:02:37 -0400

    Iterating home view

--- MessageCenter/UI/Home/HomeView.swift
+++ MessageCenter/UI/Home/HomeView.swift
@@ -9,28 +9,51 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @ObservedObject var viewModel = MessagesViewModel(
-        messagesAPI: MessagesAPI(forType: .mock)
-    )
+    @FocusState var emailTextFieldFocusState: Bool
+    @State var emailIdString = ""
+    @State var showEmailAlert = false
     
     var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 50)
-            
-            Image("roomsToGo")
-                .padding()
-            
-            Text("messageCenter")
-                .font(FontPalette.largeRegular.font)
-                .padding(.bottom, 30)
-            
-            Text("enterYourEmail")
-                .font(FontPalette.mediumRegular.font)
-            
-            Spacer()
+        NavigationStack {
+            VStack {
+                Spacer()
+                    .frame(height: 50)
+                
+                Image("roomsToGo")
+                    .padding()
+                
+                Text("messageCenter")
+                    .font(FontPalette.largeRegular.font)
+                    .padding(.bottom, 30)
+                
+                Text("enterYourEmailToSearch")
+                    .font(FontPalette.mediumRegular.font)
+                    .padding(.bottom, 20)
+                
+                UnderLinedTextField(emailIdString: $emailIdString)
+                    .focused($emailTextFieldFocusState)
+                    .padding(.horizontal, 30)
+                
+                Button("GetMessages") {
+                    if !emailIdString.isValidEmailId() {
+                        showEmailAlert = true
+                    }
+                }
+                .padding(.top, 30)
+                .foregroundColor(Color(hex: "#004FB5"))
+                .alert(isPresented: $showEmailAlert) {
+                    Alert(
+                        title: Text("Error"),
+                        message: Text("PleaseEnterValidEmail")
+                    )
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
+        .onTapGesture {
+            emailTextFieldFocusState = false
+        }
     }
 }
 
